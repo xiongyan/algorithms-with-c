5037993f64877373b334341ed00fb59b33d9bc51
Xiong, Yan <yan.xiong@intel.com>
2015-01-25 07:52:12 +0800

    set example: set covering
    Signed-off-by: Xiong, Yan <yan.xiong@intel.com>

---
+++ cover.h
@@ -0,0 +1,26 @@
+/**
+ * Header for Set Covering
+ */
+
+#ifndef COVER_H
+#define COVER_H
+
+#include "set.h"
+
+/**
+ * Define a structure for subsets identified by a key.
+ */
+
+typedef struct KSet_
+{
+	void *key;
+	Set set;
+}KSet;
+
+
+/**
+ * Public Interface
+ */
+
+int cover(Set *members, Set *subsets, Set *covering);
+#endif // COVER_H
