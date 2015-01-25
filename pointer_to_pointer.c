e7f08beee961526f87cc11b166cacff1acb4ef07
Xiong, Yan <yan.xiong@intel.com>
2015-01-25 23:51:32 +0800

    pointers to pointers as parameters
    Signed-off-by: Xiong, Yan <yan.xiong@intel.com>

---
+++ pointer_to_pointer.c
@@ -0,0 +1,34 @@
+/**
+ * Pointers to Pointers as Parameters test.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * [string_edit description]
+ * @param  str [description]
+ * @return     [description]
+ */
+int string_edit(char **str)
+{
+  /**
+   * Malloc storage for the string.
+   */
+  if ((*str = (char *)malloc(sizeof(char))) == NULL)
+  {
+	return -1;
+  }
+  strcpy(*str, "pointer ot pointer");
+  return 0;
+}
+
+int main()
+{
+  char *str = (char *)malloc(sizeof(char));
+  strcpy(str, "test");
+  string_edit(&str);
+  printf("%s\n", str);
+  free(str);
+  return 0;
+}
